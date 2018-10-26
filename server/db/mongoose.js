942f97a93ecefcbe85b0dd6360aa45ed23569dcd
ShivankSahai <shivank.sahai2017@vitstudent.ac.in>
2018-10-26 07:05:06 +0530

    new

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://Tushar:tusharj045@ds221292.mlab.com:21292/all-mart' || 'mongodb://localhost:27017/TodoApp')
 
 module.exports={mongoose}
